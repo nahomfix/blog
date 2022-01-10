fbe13fc868ac97c67119c1d90debf2c77e392acb
Nahom Bekele <nahom.fix@gmail.com>
2022-01-10 22:39:21 +0300

    modify console log message

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   rules: {
     indent: ['error', 'tab'],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
   },
--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on port: ${process.env.PORT}`);
+  console.log(`Server running on port: ${process.env.PORT}`);
 });
