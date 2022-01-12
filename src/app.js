cbbf43e8e44472a95bd58d7fe0142e87002e7d38
Nahom Bekele <nahom.fix@gmail.com>
2022-01-12 15:08:17 +0300

    fix: improve error handling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 const router = express.Router();
 const articleRoute = require('./routes/v1/article.route');
+const authRoute = require('./routes/v1/user.route');
+const { errorHandler } = require('./middlewares/error.middleware');
 
+app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 
+router.use('/auth', authRoute);
 router.use('/articles', articleRoute);
+router.use('/uploads', express.static(path.resolve(__dirname, './uploads')));
+
+console.log(path.resolve(__dirname, './uploads'));
 
 // check server online
 router.get('/', (req, res) => {
@@ -14,5 +27,7 @@ router.get('/', (req, res) => {
 });
 
 app.use('/api/v1', router);
+
+app.use(errorHandler);
 
 module.exports = app;
